eca6c88ba763d4b1ea9bbf16006279903c949e1f
Loïc Lefloch <l.lefloch.91@gmail.com>
2021-12-27 00:13:50 +0100

    update spells module

--- app/modules/api/useSpell.js
+++ app/modules/api/useSpell.js
@@ -1,7 +1,11 @@
+import { useState, useDebugValue } from 'react'
 import spells from '../../../database/data/spells.json'
 
 function useSpell(name) {
-  return { data: spells.find(spell => spell.name === name) }
+  const [spell, setSpell] = useState(spells.find(spell => spell.name === name))
+  useDebugValue(spell.name)
+
+  return { data: spell }
 }
 
 export default useSpell
