7d465595f00c4aa0aaca12bbcbfb6bb0cf14cb59
Loic Lefloch <l.lefloch.91@gmail.com>
2022-03-31 19:31:43 +0200

    better levelling table

--- components/rule-sections/LevellingTable.js
+++ components/rule-sections/LevellingTable.js
@@ -2,16 +2,22 @@ import { getLevellingStages } from "../../modules/levelling"
 import { map } from "lodash"
 
 function LevellingTable() {
+	const levellingStages  = getLevellingStages()
 	return (
-		<div className="divide divide-y w-28">
-			{map(getLevellingStages(), (requiredXp, level) => (
-				<div className="flex">
-					<div className="w-10">
+		<div className="divide divide-y">
+			{map(levellingStages, (requiredXp, level) => (
+				<div className="flex" key={level}>
+					<div className="w-10 pl-4">
 						{level}
 					</div>
-					<div className="text-right w-16">
+					<div className="text-right w-16 ml-6">
 						{requiredXp}
 					</div>
+					{level > 1 && (
+						<div className="text-right w-16 ml-6">
+							+{requiredXp - levellingStages[level - 1]}
+						</div>
+					)}
 				</div>
 			))}
 		</div>
