e1b8fd559351f1decc5aa8a0034d3cdb7ebba81e
Loïc Lefloch <l.lefloch.91@gmail.com>
2022-04-18 19:58:38 +0200

    add characters fixtures imports

--- database/fixtures/characters/index.js
+++ database/fixtures/characters/index.js
@@ -1,24 +1,53 @@
-import paladinLevel1 from "./paladin_level_1.json"
-import paladinLevel2 from "./paladin_level_2.json"
-import paladinLevel3 from "./paladin_level_3.json"
-import druidLevel1 from "./druid_level_1.json"
-import druidLevel2 from "./druid_level_2.json"
-import druidLevel3 from "./druid_level_3.json"
+
 import bardLevel1 from "./bard_level_1.json"
 import bardLevel2 from "./bard_level_2.json"
 import bardLevel3 from "./bard_level_3.json"
+
+import druidLevel1 from "./druid_level_1.json"
+import druidLevel2 from "./druid_level_2.json"
+import druidLevel3 from "./druid_level_3.json"
+
+import fighterLevel1 from "./fighter_level_1.json"
+import fighterLevel2 from "./fighter_level_2.json"
+import fighterLevel3 from "./fighter_level_3.json"
+
+import paladinLevel1 from "./paladin_level_1.json"
+import paladinLevel2 from "./paladin_level_2.json"
+import paladinLevel3 from "./paladin_level_3.json"
+
+import rogueLevel1 from "./rogue_level_1.json"
+import rogueLevel2 from "./rogue_level_2.json"
+import rogueLevel3 from "./rogue_level_3.json"
+
+import wizardLevel1 from "./wizard_level_1.json"
+import wizardLevel2 from "./wizard_level_2.json"
+
 import ylvir from "./ylvir.json"
 
 const characters = [
 	bardLevel1,
 	bardLevel2,
 	bardLevel3,
-	paladinLevel1,
-	paladinLevel2,
-	paladinLevel3,
+
 	druidLevel1,
 	druidLevel2,
 	druidLevel3,
+
+	paladinLevel1,
+	paladinLevel2,
+	paladinLevel3,
+
+	fighterLevel1,
+	fighterLevel2,
+	fighterLevel3,
+
+	rogueLevel1,
+	rogueLevel2,
+	rogueLevel3,
+
+	wizardLevel1,
+	wizardLevel2,
+
 	ylvir,
 ]
 export default characters
